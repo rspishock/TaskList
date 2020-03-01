b43184e1a216af577369374dc9cdabd0e41bf909
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-03-01 14:56:57 -0500

    Initial Commit

---
+++ TaskList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskList
+//
+//  Created by Ryan Spishock on 3/1/20.
+//  Copyright © 2020 Ryan Spishock. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
